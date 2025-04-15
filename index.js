c70d02ae74cae7820f18c8d1ac2fd59ccd618a41
Alex <a.gulbickij@gmail.com>
2025-04-15 15:05:57 +0300

    steps for add device flow

--- index.js
+++ index.js
@@ -6,26 +6,16 @@ const __dirname = path.resolve();
 
 const app = express();
 
-console.log('__dirname', __dirname);
 app.engine(
   'hbs',
   engine({
     extname: '.hbs',
-    // defaultLayout:  path.join(__dirname, './views/layouts/main'),
     layoutsDirDir: path.resolve(__dirname, './views/layouts/'),
     partialsDir: path.resolve(__dirname, './views/partials/'),
   }),
 );
 app.set('view engine', 'hbs');
 app.set('views', path.resolve(__dirname, './views'));
-
-// app.set('views', path.join(__dirname, './views'));
-
-// hbs.getPartials().then(function (partials) {
-//   console.log(partials);
-//   // => { 'foo/bar': [Function],
-//   // =>    title: [Function] }
-// });
 
 app.get('/', (req, res) => {
   res.render('map', { activeLink: 'home', layout: 'with-map' });
@@ -53,6 +43,10 @@ app.get('/admin', (req, res) => {
 
 app.get('/project/device', (req, res) => {
   res.render('device', { activeLink: 'projects', layout: 'main' });
+});
+
+app.get('/project/add-device', (req, res) => {
+  res.render('add-device', { activeLink: 'projects', layout: 'main' });
 });
 
 app.use(express.static('public'));
--- public/form.js
+++ public/form.js
@@ -24,3 +24,33 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+
+function getCurrentCoordinates() {
+  const output = document.getElementById('output');
+
+  const latInput = document.getElementById('lat-input');
+  const lngInput = document.getElementById('lng-input');
+
+  if (!navigator.geolocation) {
+    output.textContent = 'Geolocation is not supported by your browser.';
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      latInput.value = latitude;
+      lngInput.value = longitude;
+    },
+    function (error) {
+      console.log(`Error getting location: ${error.message}`);
+    },
+  );
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document
+    .getElementById('getCoordinates')
+    .addEventListener('click', getCurrentCoordinates);
+});
